458dfde33ef5345ed8082a97c9a195daba1da141
Wang Lei <sevenmega@gmail.com>
2019-09-01 22:32:54 +0800

    update interpreter with bn/scale/eltwise etc

--- lib/Dialect/TPU/Interpreter/TpuInterpreter.cpp
+++ lib/Dialect/TPU/Interpreter/TpuInterpreter.cpp
@@ -86,7 +86,11 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
       } else {
         llvm::errs() << "    found in map\n";
         auto vec = it->second.get();
-        llvm::errs() << "      vec size = " << vec->size() << "\n";
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
       }
       operandIdx++;
     }
@@ -117,7 +121,11 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
       } else {
         llvm::errs() << "    found in map\n";
         auto vec = it->second.get();
-        llvm::errs() << "      vec size = " << vec->size() << "\n";
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
       }
     }
     auto result = op.getResult();
@@ -147,7 +155,11 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
       } else {
         llvm::errs() << "    found in map\n";
         auto vec = it->second.get();
-        llvm::errs() << "      vec size = " << vec->size() << "\n";
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
       }
     }
     auto result = op.getResult();
@@ -177,7 +189,11 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
       } else {
         llvm::errs() << "    found in map\n";
         auto vec = it->second.get();
-        llvm::errs() << "      vec size = " << vec->size() << "\n";
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
       }
       operandIdx++;
     }
@@ -194,16 +210,139 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
 
     return success();
   }
-  if (auto reluOp = dyn_cast<tpu::ReluOp>(opInst)) {
+  if (auto op = dyn_cast<tpu::ReluOp>(opInst)) {
     llvm::errs() << "ReluOp" << "\n";
+    //op.dump();
+    {
+      auto operand = op.getOperand();
+      llvm::errs() << "  operand[0] "; operand->getType().dump(); llvm::errs() << "\n";
+      // find operand in valueMapping
+      auto it = valueMapping.find(operand);
+      if (it == valueMapping.end()) {
+        llvm::errs() << "    didn't find\n";
+        assert(0);
+      } else {
+        llvm::errs() << "    found in map\n";
+        auto vec = it->second.get();
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
+      }
+    }
+    auto result = op.getResult();
+    llvm::errs() << "  result "; result->getType().dump(); llvm::errs() << "\n";
+    std::vector<int64_t> shape = result->getType().cast<TensorType>().getShape();
+    assert(shape.size() <= 4);
+    auto size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<>());;
+    auto result_data = std::make_unique<std::vector<float> >(size);
+
+    // TODO: do the actual compute here
+
+    valueMapping[result] = std::move(result_data);
     return success();
   }
-  if (auto batchNormOp = dyn_cast<tpu::BatchNormOp>(opInst)) {
+  if (auto op = dyn_cast<tpu::BatchNormOp>(opInst)) {
     llvm::errs() << "BatchNormOp" << "\n";
+    //op.dump();
+    unsigned int operandIdx = 0;
+    for (auto *operand : op.getOperands()) {
+      llvm::errs() << "  operand[" << operandIdx << "] "; operand->getType().dump(); llvm::errs() << "\n";
+      // find operand in valueMapping
+      auto it = valueMapping.find(operand);
+      if (it == valueMapping.end()) {
+        llvm::errs() << "    didn't find\n";
+        assert(0);
+      } else {
+        llvm::errs() << "    found in map\n";
+        auto vec = it->second.get();
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
+      }
+      operandIdx++;
+    }
+    auto result = op.getResult();
+    llvm::errs() << "  result "; result->getType().dump(); llvm::errs() << "\n";
+    std::vector<int64_t> shape = result->getType().cast<TensorType>().getShape();
+    assert(shape.size() <= 4);
+    auto size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<>());;
+    auto result_data = std::make_unique<std::vector<float> >(size);
+
+    // TODO: do the actual compute here
+
+    valueMapping[result] = std::move(result_data);
     return success();
   }
-  if (auto scaleOp = dyn_cast<tpu::ScaleOp>(opInst)) {
+  if (auto op = dyn_cast<tpu::ScaleOp>(opInst)) {
     llvm::errs() << "ScaleOp" << "\n";
+    //op.dump();
+    unsigned int operandIdx = 0;
+    for (auto *operand : op.getOperands()) {
+      llvm::errs() << "  operand[" << operandIdx << "] "; operand->getType().dump(); llvm::errs() << "\n";
+      // find operand in valueMapping
+      auto it = valueMapping.find(operand);
+      if (it == valueMapping.end()) {
+        llvm::errs() << "    didn't find\n";
+        assert(0);
+      } else {
+        llvm::errs() << "    found in map\n";
+        auto vec = it->second.get();
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
+      }
+      operandIdx++;
+    }
+    auto result = op.getResult();
+    llvm::errs() << "  result "; result->getType().dump(); llvm::errs() << "\n";
+    std::vector<int64_t> shape = result->getType().cast<TensorType>().getShape();
+    assert(shape.size() <= 4);
+    auto size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<>());;
+    auto result_data = std::make_unique<std::vector<float> >(size);
+
+    // TODO: do the actual compute here
+
+    valueMapping[result] = std::move(result_data);
+    return success();
+  }
+  if (auto op = dyn_cast<tpu::EltwiseOp>(opInst)) {
+    llvm::errs() << "ScaleOp" << "\n";
+    //op.dump();
+    unsigned int operandIdx = 0;
+    for (auto *operand : op.getOperands()) {
+      llvm::errs() << "  operand[" << operandIdx << "] "; operand->getType().dump(); llvm::errs() << "\n";
+      // find operand in valueMapping
+      auto it = valueMapping.find(operand);
+      if (it == valueMapping.end()) {
+        llvm::errs() << "    didn't find\n";
+        assert(0);
+      } else {
+        llvm::errs() << "    found in map\n";
+        auto vec = it->second.get();
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
+      }
+      operandIdx++;
+    }
+    auto result = op.getResult();
+    llvm::errs() << "  result "; result->getType().dump(); llvm::errs() << "\n";
+    std::vector<int64_t> shape = result->getType().cast<TensorType>().getShape();
+    assert(shape.size() <= 4);
+    auto size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<>());;
+    auto result_data = std::make_unique<std::vector<float> >(size);
+
+    // TODO: do the actual compute here
+
+    valueMapping[result] = std::move(result_data);
     return success();
   }
   if (auto op = dyn_cast<tpu::ReshapeOp>(opInst)) {
@@ -220,7 +359,11 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
       } else {
         llvm::errs() << "    found in map\n";
         auto vec = it->second.get();
-        llvm::errs() << "      vec size = " << vec->size() << "\n";
+        if (vec) {
+          llvm::errs() << "      vec size = " << vec->size() << "\n";
+        } else {
+          llvm::errs() << "      vec is nullptr\n";
+        }
       }
     }
     auto result = op.getResult();
@@ -233,6 +376,20 @@ LogicalResult ModuleInterpreter::runOperation(Operation &opInst) {
     // TODO: do the actual compute here
 
     valueMapping[result] = std::move(result_data);
+
+    return success();
+  }
+
+  if (auto op = dyn_cast<ConstantOp>(opInst)) {
+    llvm::errs() << "ConstantOp" << "\n";
+    //op.dump();
+    // TODO: use specific Op for null operand
+    // only support zero constant for now
+    // TODO: check isZero
+
+    // it it safe to ignore, put null pointer to the valueMapping
+    auto result = op.getResult();
+    valueMapping[result] = std::move(nullptr);
 
     return success();
   }
