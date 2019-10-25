594061a6433095cbf07e4edd5de782cc44cf7cba
Lei Wang <sevenmega@gmail.com>
2019-10-25 22:58:53 +0800

    put native cpu implementation into a separate file

---
+++ include/mlir/Dialect/TPU/NativeCpuImplementation.h
@@ -0,0 +1,30 @@
+#ifndef MLIR_DIALECT_TPU_NATIVE_CPU_IMPLEMENTATION_H_
+#define MLIR_DIALECT_TPU_NATIVE_CPU_IMPLEMENTATION_H_
+
+int mkldnn_conv(float *input, float *weight, float *bias,
+    float *output, int n, int ic, int ih, int iw, int oc, int oh, int ow,
+    int kh, int kw, int sh, int sw, int ph, int pw);
+
+int mkldnn_pool(float *input, float *output,
+    int n, int c, int ih, int iw, int oh, int ow,
+    int kh, int kw, int sh, int sw, int ph, int pw,
+    bool is_avg);
+
+int mkldnn_ip(float *input, float *weight, float *bias,
+    float *output, int m, int k, int n, bool transpose);
+
+int my_relu(float *input, float *output,
+    int n, int c, int h, int w, float negative_slope);
+
+int my_bn(float *input, float *mean, float *variance, float *scale,
+    float *output, int n, int c, int h, int w);
+
+int my_scale(float *input, float *scale, float *bias,
+    float *output, int n, int c, int h, int w);
+
+int my_softmax(float *input, float *output, int n, int c);
+
+int my_eltwise(float *input_1, float *input_2, float *output,
+    int n, int c, int h, int w, int op);
+
+#endif // MLIR_DIALECT_TPU_NATIVE_CPU_IMPLEMENTATION_H_
