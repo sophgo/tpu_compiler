d9aa19a27c4e828735dfc401afbeb6ac95221cf7
Wang Lei <sevenmega@gmail.com>
2019-11-24 13:21:18 +0800

    update for bf16 regression with full cmdbuf pass

--- regression_5_bf16_cmdbuf.sh
+++ regression_5_bf16_cmdbuf.sh
@@ -70,19 +70,16 @@ export LD_LIBRARY_PATH=$TPU_BASE_DIR/install_cmodel/lib:$LD_LIBRARY_PATH
     out_all.bin \
     32921552 0 32921552 1
 python ../llvm/projects/mlir/externals/python_tools/bin_extract.py \
-    out_all.bin out_fc1000.bin bf16 0x00049800 1000
-
-if false; then
-
-diff out_fc1000.bin $DATA_DIR/test_cat_out_fc1000-bf16.bin
+    out_all.bin out_fc1000_bf16.bin bf16 0x00049800 1000
+diff out_fc1000_bf16.bin $DATA_DIR/test_cat_out_fc1000-bf16.bin
+# python ../llvm/projects/mlir/externals/python_tools/bin_bf16_to_fp32.py out_fc1000_bf16.bin out_fc1000_fp32.bin
+# python ../llvm/projects/mlir/externals/python_tools/bin_dump.py out_fc1000_fp32.bin float32 1 1 1 1000 5
 
 # compare all tensors
 python ../llvm/projects/mlir/externals/python_tools/bin_to_npz.py \
     out_all.bin neuron_map_bf16.csv out_all_bf16.npz
 python ../llvm/projects/mlir/externals/python_tools/npz_compare.py \
     out_all_bf16.npz tensor_all_quant-bf16.npz show 5
-
-fi
 
 # VERDICT
 echo $0 PASSED
