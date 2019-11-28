b8b5bde6a753da112bc920256ab29dbedd15843e
leon.lee <leon.lee@wisecore.com.tw>
2019-11-28 16:55:35 +0800

    Support bf16 depthwise weight
    Support depthwise weight shape.

--- lib/Dialect/TPU/Quantization/QuantizeBf16.cpp
+++ lib/Dialect/TPU/Quantization/QuantizeBf16.cpp
@@ -82,11 +82,21 @@ struct TpuQuantConv2DOpPattern : public RewritePattern {
     // create new tensors for quantized fliter and bias
     auto filter_type = convOp.filter()->getType().cast<TensorType>();
     std::vector<int64_t> filter_shape(filter_type.getShape());
-    assert(filter_shape.size() == 4);
     int64_t filter_size = std::accumulate(std::begin(filter_shape),
         std::end(filter_shape), 1, std::multiplies<>());
     assert(filter_size == (int64_t)weights[0]->size());
-    int64_t oc = filter_shape[0];
+
+    int64_t oc = 0;
+    if (filter_shape.size() == 4) {
+      oc = filter_shape[0];
+    } else if (filter_shape.size() == 5) {
+      assert(convOp.group() != 1);
+      // g, oc/g, ic/g, kh, kw
+      oc = filter_shape[0] * filter_shape[1];
+    } else {
+      assert(0);
+    }
+
     std::vector<bfloat16> new_filter(filter_size);
     std::vector<bfloat16> new_bias(oc);
 
