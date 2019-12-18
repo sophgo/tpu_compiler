68ccfda29256bd078cf1ef4fccf9690cf5aa47b4
Lei Wang <sevenmega@gmail.com>
2019-12-18 11:18:13 +0800

    update envsetup with more PYTHONPATH

--- envsetup.sh
+++ envsetup.sh
@@ -31,6 +31,7 @@ export CMODEL_PATH=$TPU_BASE/install_cmodel
 export SUPPORT_PATH=$TPU_BASE/install_support
 export BMBUILDER_PATH=$TPU_BASE/install_bmbuilder
 export RUNTIME_PATH=$TPU_BASE/install_runtime
+export PYTHON_TOOLS_PATH=$MLIR_SRC_PATH/externals/python_tools
 
 export MLIR_PATH=$TPU_BASE/install_mlir
 
@@ -39,12 +40,13 @@ export LD_LIBRARY_PATH=$CMODEL_PATH/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$SUPPORT_PATH/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$BMBUILDER_PATH/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$MKLDNN_PATH/lib:$LD_LIBRARY_PATH
-# export LD_LIBRARY_PATH=$CAFFE_PATH/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CAFFE_PATH/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$CAFFE_PATH/mkl/mklml/lib:$LD_LIBRARY_PATH
 
-export PATH=$MLIR_SRC_PATH/externals/python_tools:$PATH
 export PATH=$TPU_BASE/llvm-project/build/bin:$PATH
-
-# for pymlir wrapper and tools
-export PYTHONPATH=$TPU_BASE/llvm-project/build/lib:$PYTHONPATH
+export PATH=$MLIR_SRC_PATH/externals/python_tools:$PATH
 export PATH=$MLIR_SRC_PATH/bindings/python/tools:$PATH
+
+export PYTHONPATH=$TPU_BASE/llvm-project/build/lib:$PYTHONPATH
+export PYTHONPATH=$PYTHON_TOOLS_PATH:$PYTHONPATH
+export PYTHONPATH=$CAFFE_PATH/python:$PYTHONPATH
