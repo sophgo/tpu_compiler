7ced8df334ad94ae8af1ce30cc18f2597a680b74
Lei Wang <lei.wang@cvitek.com>
2020-02-01 00:19:42 +0800

    update regression script

--- regression/run_regression.sh
+++ regression/run_regression.sh
@@ -3,6 +3,18 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+net_list=(
+  "resnet50"
+  "inception_v4"
+  "yolo_v3"
+)
+
+if [ ! -z "$1" ]; then
+  $DIR/$1/regression_$1.sh
+  echo "regression $1 PASSED"
+  exit 0
+fi
+
 if [ ! -e regression_out ]; then
   mkdir regression_out
 fi
@@ -11,9 +23,11 @@ pushd regression_out
 # clear previous output
 rm -f *.mlir *.bin *.npz *.csv
 
-$DIR/resnet50/regression_resnet50.sh
-$DIR/yolo_v3/regression_yolo_v3.sh
-# $DIR/ssd300/regression_ssd300.sh
+for net in ${net_list[@]}
+do
+  echo "regression $net"
+  $DIR/$net/regression_$net.sh
+done
 
 popd
 
