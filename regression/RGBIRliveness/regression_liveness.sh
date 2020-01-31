512b57fe5d6bf51f13ede37f3fc782164b8c3b8a
Terry Tang <terry.tang@bitmain.com>
2020-01-31 11:47:55 +0800

    Add liveness int8 and cmdbuf regression.

--- regression/RGBIRliveness/regression_liveness.sh
+++ regression/RGBIRliveness/regression_liveness.sh
@@ -15,7 +15,7 @@ rm -f *.mlir *.bin *.npz *.csv
 # run tests
 $DIR/regression_liveness_0_fp32.sh
 $DIR/regression_liveness_1_int8.sh
-#$DIR/regression_resnet50_3_int8_cmdbuf.sh
+$DIR/regression_liveness_2_int8_cmdbuf.sh
 #$DIR/regression_resnet50_4_bf16.sh
 #$DIR/regression_resnet50_5_bf16_cmdbuf.sh
 
