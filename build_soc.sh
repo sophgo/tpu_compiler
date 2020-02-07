56123c0c308286e579dd03bf486b8f9912f09764
leon.lee <leon.lee@wisecore.com.tw>
2020-02-07 20:21:48 +0800

    Build arm version of flatbuffer
    Build flatbuffer and test_cvinet.

--- build_soc.sh
+++ build_soc.sh
@@ -11,6 +11,41 @@ export RUNTIME_PATH=$TPU_BASE/install_soc_runtime
 
 export TOOLCHAIN_FILE_PATH=$TPU_BASE/llvm-project/llvm/projects/mlir/externals/runtime/scripts/toolchain-aarch64-linux.cmake
 
+# build host flatbuffers
+if [ ! -e $MLIR_SRC_PATH/third_party/flatbuffers/build ]; then
+  mkdir $MLIR_SRC_PATH/third_party/flatbuffers/build
+fi
+pushd $MLIR_SRC_PATH/third_party/flatbuffers/build
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$FLATBUFFERS_PATH ..
+cmake --build . --target install
+popd
+pushd $MLIR_SRC_PATH/third_party/flatbuffers
+cp -a python $FLATBUFFERS_PATH/
+popd
+
+# generate target-independent flatbuffer schema
+if [ ! -e $CVIBUILDER_PATH ]; then
+  mkdir $CVIBUILDER_PATH
+  mkdir $CVIBUILDER_PATH/include
+fi
+pushd $CVIBUILDER_PATH/include
+flatc --cpp --gen-object-api $MLIR_SRC_PATH/externals/cvibuilder/src/cvimodel.fbs
+popd
+pushd $MLIR_SRC_PATH/externals/cvibuilder
+cp -a python $CVIBUILDER_PATH/
+popd
+
+# build target flat buffer
+# not build test since it will trigger self test
+export FLATBUFFERS_PATH=$TPU_BASE/install_soc_flatbuffers
+if [ ! -e $MLIR_SRC_PATH/third_party/flatbuffers/build_soc ]; then
+  mkdir $MLIR_SRC_PATH/third_party/flatbuffers/build_soc
+fi
+pushd $MLIR_SRC_PATH/third_party/flatbuffers/build_soc
+CC=aarch64-linux-gnu-gcc CXX=aarch64-linux-gnu-g++ cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$FLATBUFFERS_PATH -DFLATBUFFERS_BUILD_TESTS=OFF ..
+cmake --build . --target install
+popd
+
 # build bmkernel
 if [ ! -e $MLIR_SRC_PATH/externals/bmkernel/build_soc ]; then
   mkdir $MLIR_SRC_PATH/externals/bmkernel/build_soc
@@ -48,6 +83,7 @@ cmake -DCHIP=BM1880v2 -DRUNTIME=SOC \
 	-DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE_PATH \
 	-DSUPPORT_PATH=$SUPPORT_PATH \
 	-DBMKERNEL_PATH=$BMKERNEL_PATH \
+        -DFLATBUFFERS_PATH=$FLATBUFFERS_PATH -DCVIBUILDER_PATH=$CVIBUILDER_PATH \
 	-DCMAKE_INSTALL_PREFIX=$RUNTIME_PATH ..
 cmake --build . --target install
 popd
