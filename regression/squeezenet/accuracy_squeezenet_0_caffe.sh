b3099015f3028d0d2d19a4665ea90f713f548e61
quan.li <quan.li@cviteck.com>
2020-02-18 08:09:47 +0800

    update squeezenet calibration table and accuracy

---
+++ regression/squeezenet/accuracy_squeezenet_0_caffe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+source $DIR/../../envsetup.sh
+
+EVAL_FUNC=eval_caffe_classifier.py
+
+$EVAL_FUNC \
+    --model_def $MODEL_PATH/imagenet/squeezenet/caffe/deploy_v1.1.prototxt \
+    --pretrained_model $MODEL_PATH/imagenet/squeezenet/caffe/squeezenet_v1.1.caffemodel \
+    --dataset $DATASET_PATH \
+    --mean_file $PYTHON_TOOLS_PATH/data/ilsvrc_2012_mean.npy \
+    --count=$1
+
+echo $0 DONE
