9e364352c04d3b7b079bb13ce549735a7a04b868
Lei Wang <lei.wang@cvitek.com>
2020-02-19 13:36:21 +0800

    update yolo_v3 regression

--- regression/yolo_v3/regression_yolo_v3_1_fp32.sh
+++ regression/yolo_v3/regression_yolo_v3_1_fp32.sh
@@ -4,22 +4,48 @@ set -e
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 source $DIR/../../envsetup.sh
 
+CHECK_NON_OPT_VERSION=0
+
 # translate from caffe model
 mlir-translate \
     --caffe-to-mlir $MODEL_PATH/object_detection/yolo_v3/caffe/416/yolov3_416.prototxt \
     --caffemodel $MODEL_PATH/object_detection/yolo_v3/caffe/416/yolov3_416.caffemodel \
     -o yolo_v3_416.mlir
 
-# assign layer_id right away, and output op_info
+if [ $CHECK_NON_OPT_VERSION -eq 1 ]; then
+  mlir-opt \
+      --assign-layer-id \
+      --print-tpu-op-info \
+      --tpu-op-info-filename yolo_v3_op_info.csv \
+      yolo_v3_416.mlir \
+      -o dummy.mlir
+  # test mlir interpreter
+  mlir-tpu-interpreter yolo_v3_416.mlir \
+      --tensor-in yolo_v3_in_fp32.npz \
+      --tensor-out yolo_v3_out_fp32.npz \
+      --dump-all-tensor=yolo_v3_tensor_all_fp32.npz
+  npz_compare.py yolo_v3_out_fp32.npz yolo_v3_out_fp32_ref.npz -v
+  npz_compare.py \
+      yolo_v3_tensor_all_fp32.npz \
+      yolo_v3_blobs.npz \
+      --op_info yolo_v3_op_info.csv \
+      --tolerance=0.9999,0.9999,0.999 -vv
+fi
+
+# assign layer_id right away, and apply all frontend optimizations
 mlir-opt \
     --assign-layer-id \
     --print-tpu-op-info \
     --tpu-op-info-filename yolo_v3_op_info.csv \
+    --convert-bn-to-scale \
+    --fold-scale \
+    --merge-scale-into-conv \
+    --fuse-relu \
     yolo_v3_416.mlir \
-    -o yolo_v3_416_id.mlir
+    -o yolo_v3_416_opt.mlir
 
 # test mlir interpreter
-mlir-tpu-interpreter yolo_v3_416.mlir \
+mlir-tpu-interpreter yolo_v3_416_opt.mlir \
     --tensor-in yolo_v3_in_fp32.npz \
     --tensor-out yolo_v3_out_fp32.npz \
     --dump-all-tensor=yolo_v3_tensor_all_fp32.npz
@@ -28,21 +54,7 @@ npz_compare.py \
     yolo_v3_tensor_all_fp32.npz \
     yolo_v3_blobs.npz \
     --op_info yolo_v3_op_info.csv \
-    --tolerance=0.9999,0.9999,0.999 -vvv
-
-# apply all possible pre-calibration optimizations
-mlir-opt \
-    --convert-bn-to-scale \
-    --fold-scale \
-    --merge-scale-into-conv \
-    yolo_v3_416_id.mlir \
-    -o yolo_v3_416_opt.mlir
-
-# test opt
-mlir-tpu-interpreter yolo_v3_416_opt.mlir \
-    --tensor-in yolo_v3_in_fp32.npz \
-    --tensor-out yolo_v3_opt_out_fp32.npz
-npz_compare.py yolo_v3_opt_out_fp32.npz yolo_v3_out_fp32_ref.npz -v
+    --tolerance=0.9999,0.9999,0.999 -vv
 
 # VERDICT
 echo $0 PASSED
