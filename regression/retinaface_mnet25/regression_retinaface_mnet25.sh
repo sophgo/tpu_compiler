059f58a471a58903c1e1aa9e7a8f0c8b57947e9c
rocking <rocking@bitmain.com>
2020-02-25 12:12:08 +0800

    Add retinaface-mobilenet caffe regression

---
+++ regression/retinaface_mnet25/regression_retinaface_mnet25.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+NET=retinaface_mnet25
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ ! -e $NET ]; then
+  mkdir $NET
+fi
+
+pushd $NET
+# clear previous output
+rm -f *.mlir *.bin *.npz *.csv *threshold_table *.txt
+
+# run tests
+$DIR/regression_retinaface_mnet25_0_caffe.sh
+# $DIR/regression_retinaface_mnet25_1_fp32.sh
+# $DIR/regression_retinaface_mnet25_2_int8.sh
+
+popd
+
+# VERDICT
+echo $0 PASSED
