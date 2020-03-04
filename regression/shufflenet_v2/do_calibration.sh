0554498bd97f00903e1405680810029e1f917183
charle.hu <charle.hu@cvitek.com>
2020-03-04 15:39:42 +0800

    refine shufflenet_v2 regression
    use new python tool, and remove useless file

--- regression/shufflenet_v2/do_calibration.sh
+++ regression/shufflenet_v2/do_calibration.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-CALI_PATH=$DIR/../../externals/calibration_tool
-source $DIR/../../envsetup.sh
-mkdir -p lib
-cp $CALI_PATH/build/calibration_math.so ./lib/
+source $MLIR_SRC_PATH/envsetup.sh
+CALI_PATH=$REGRESSION_PATH/shufflenet_v2
 
 #input txt
 #python3 gen_data_list.py ~/data/dataset/imagenet/img_val_extracted/val 5000 input.txt
 
-python $CALI_PATH/run_calibration.py shufflenet_v2 shufflenet_opt.mlir ./data/input.txt \
-            --input_num=1000 \
-            --out_path ./data
+  python $TPU_PYTHON_PATH/run_calibration.py \
+    $CALI_PATH/shufflenet/shufflenet_opt.mlir \
+    $CALI_PATH/data/input.txt \
+    --output_file=$CALIPATH/data/shufflenet_v2_threshold_table \
+    --images_dim 224,224 \
+    --raw_scale 1.0 \
+    --input_num=1000
