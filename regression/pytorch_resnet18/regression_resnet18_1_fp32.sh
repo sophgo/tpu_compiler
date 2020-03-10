359b2397fb7be75e57a2166a180b0737f707dc87
sam.zheng <sam.zheng@wisecore.com.tw>
2020-03-10 15:24:33 +0800

    fix resnet 18 pytorch regression
    Change-Id: I3766530aa49359ecd796bd13838b0cc8820983c2

---
+++ regression/pytorch_resnet18/regression_resnet18_1_fp32.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+source $DIR/../../envsetup.sh
+
+
+convert.py \
+    --model_path $MODEL_PATH/imagenet/resnet/onnx/resnet18.onnx \
+    --model_name resnet18 \
+    --model_type onnx \
+    --mlir_file_path resnet18.mlir
+
+mlir-opt \
+    --assign-layer-id \
+    --canonicalize \
+    --convert-bn-to-scale \
+    --print-tpu-op-info \
+    --tpu-op-info-filename resnet18_op_info.csv \
+    resnet18.mlir \
+    -o resnet18_opt.mlir
+
+# test frontend optimizations
+mlir-tpu-interpreter resnet18_opt.mlir \
+    --tensor-in res18_in_fp32.npz \
+    --tensor-out resnet18_out_fp32.npz \
+    --dump-all-tensor=resnet18_tensor_all_fp32.npz
+
+npz_compare.py resnet18_out_fp32.npz res18_out_onnx.npz -vvv
+
+
+# VERDICT
+echo $0 PASSED
