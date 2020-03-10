2c183b0c2ed301c2be5321828207405623555aea
sam.zheng <sam.zheng@wisecore.com.tw>
2020-03-10 15:30:19 +0800

    add pose data
    Change-Id: I60ecfb35f90514ac850f91bd5a107933b40324bf

--- regression/pytorch_resnet18/regression_resnet18_0_onnx.sh
+++ regression/pytorch_resnet18/regression_resnet18_0_onnx.sh
@@ -6,10 +6,10 @@ source $DIR/../../envsetup.sh
 
 # onnx inference only on python3, pls pip3 install onnxruntime
 run_onnx_inference.py \
-    --input_file data/dog.npz \
+    --input_file $DIR/data/dog.npz \
     --output_file res18_out_onnx.npz \
     --model_path $MODEL_PATH/imagenet/resnet/onnx/resnet18.onnx
     
-npz_extract.py data/dog.npz res18_in_fp32.npz input
+npz_extract.py $DIR/data/dog.npz res18_in_fp32.npz input
 # VERDICT
 echo $0 PASSED
