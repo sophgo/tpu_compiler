5d8b7e15facf4cf39ee12f79ad48de5fedf2f8c7
charle.hu <charle.hu@cvitek.com>
2020-03-17 17:49:23 +0800

    cleanup the code
    remove useless code and do some small fix
    
    Change-Id: Ieec242968da42568e42167fa585d3b3ce6365f55

--- regression/shufflenet_v1/regression_shufflenet_2_int8.sh
+++ regression/shufflenet_v1/regression_shufflenet_2_int8.sh
@@ -4,9 +4,6 @@ set -e
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 source $DIR/../../envsetup.sh
 
-COMPARE_ALL=1
-COMPARE_OUTPUT_BIT_TRUE=1
-
 # import calibration table
 mlir-opt \
     --import-calibration-table \
@@ -29,28 +26,12 @@ mlir-tpu-interpreter shufflenet_quant_int8_per_layer.mlir \
     --tensor-out shufflenet_out_int8_per_layer.npz \
     --dump-all-tensor=shufflenet_tensor_all_int8_per_layer.npz
 
-if [ $COMPARE_OUTPUT_BIT_TRUE -eq 1 ]; then
-  npz_to_bin.py \
-      shufflenet_tensor_all_int8_per_layer.npz \
-      fc1000 \
-      shufflenet_out_fc1000_int8_per_layer.bin \
-      int8
-  bin_compare.py \
-      shufflenet_out_fc1000_int8_per_layer.bin \
-      $REGRESSION_PATH/shufflenet_v1/data/test_cat_out_shufflenet_fc1000_int8_per_layer.bin \
-      int8 1 1 1 1000 5
-fi
-
-if [ $COMPARE_ALL -eq 1 ]; then
-  # this will fail for now, because prob has been dequantized twice, others should pass
   npz_compare.py \
       shufflenet_tensor_all_int8_per_layer.npz \
       shufflenet_blobs.npz \
       --op_info shufflenet_op_info_int8_per_layer.csv \
       --dequant \
-      --excepts prob \
-      --tolerance 0.72,0.71,-0.14 -vv
-fi
+      --tolerance 0.94,0.94,0.66 -vv
 
 ###############################################################################
 # quantization 2: per-channel int8
@@ -67,28 +48,14 @@ mlir-tpu-interpreter shufflenet_quant_int8_per_channel.mlir \
     --tensor-out shufflenet_out_int8_per_channel.npz \
     --dump-all-tensor=shufflenet_tensor_all_int8_per_channel.npz
 
-if [ $COMPARE_OUTPUT_BIT_TRUE -eq 1 ]; then
-  npz_to_bin.py \
-      shufflenet_tensor_all_int8_per_channel.npz \
-      fc1000 \
-      shufflenet_out_fc1000_int8_per_channel.bin \
-      int8
-  bin_compare.py \
-      shufflenet_out_fc1000_int8_per_channel.bin \
-      $REGRESSION_PATH/shufflenet_v1/data/test_cat_out_shufflenet_fc1000_int8_per_channel.bin \
-      int8 1 1 1 1000 5
-fi
-
-if [ $COMPARE_ALL -eq 1 ]; then
-  # this will fail for now, because prob has been dequantized twice, others should pass
-  npz_compare.py \
+npz_compare.py \
       shufflenet_tensor_all_int8_per_channel.npz \
       shufflenet_blobs.npz \
       --op_info shufflenet_op_info_int8_per_channel.csv \
       --dequant \
       --excepts prob \
-      --tolerance 0.72,0.71,-0.11 -vv
-fi
+      --tolerance 0.97,0.97,0.76 -vv
+
 
 ###############################################################################
 # quantization 3: per-channel int8 with multiplier
@@ -105,28 +72,13 @@ mlir-tpu-interpreter shufflenet_quant_int8_multiplier.mlir \
     --tensor-out shufflenet_out_int8_multiplier.npz \
     --dump-all-tensor=shufflenet_tensor_all_int8_multiplier.npz
 
-if [ $COMPARE_OUTPUT_BIT_TRUE -eq 1 ]; then
-  npz_to_bin.py \
-      shufflenet_tensor_all_int8_multiplier.npz \
-      fc1000 \
-      shufflenet_out_fc1000_int8_multiplier.bin \
-      int8
-  bin_compare.py \
-      shufflenet_out_fc1000_int8_multiplier.bin \
-      $REGRESSION_PATH/shufflenet_v1/data/test_cat_out_shufflenet_fc1000_int8_multiplier.bin \
-      int8 1 1 1 1000 5
-fi
-
-if [ $COMPARE_ALL -eq 1 ]; then
-  # this will fail for now, because prob has been dequantized twice, others should pass
-  npz_compare.py \
+npz_compare.py \
       shufflenet_tensor_all_int8_multiplier.npz \
       shufflenet_blobs.npz \
       --op_info shufflenet_op_info_int8_multiplier.csv \
       --dequant \
       --excepts prob \
-      --tolerance 0.72,0.71,-0.11 -vv
-fi
+      --tolerance 0.97,0.97,0.76 -vv
 
 # VERDICT
 echo $0 PASSED
--- regression/shufflenet_v2/regression_shufflenet_0_caffe.sh
+++ regression/shufflenet_v2/regression_shufflenet_0_caffe.sh
@@ -8,7 +8,7 @@ source $DIR/../../envsetup.sh
 run_caffe_classifier.py \
     --model_def $MODEL_PATH/imagenet/shufflenet_v2/caffe/shufflenet_v2_x0.5.prototxt \
     --pretrained_model $MODEL_PATH/imagenet/shufflenet_v2/caffe/shufflenet_v2_x0.5.caffemodel \
-    --label_file $REGRESSION_PATH/shufflenet_v2/data/ilsvrc12/synset_words.txt \
+    --label_file $REGRESSION_PATH/resnet50/data/ilsvrc12/synset_words.txt \
     --dump_blobs shufflenet_blobs.npz \
     --dump_weights shufflenet_weights.npz \
     --raw_scale 1.0 \
@@ -18,11 +18,6 @@ run_caffe_classifier.py \
 # extract input and output
 npz_extract.py shufflenet_blobs.npz shufflenet_in_fp32.npz data
 npz_extract.py shufflenet_blobs.npz shufflenet_out_fp32_fc.npz fc
-
-# fix input data consistency
-# because jpeg decoder may introduce difference, use save file to overwrite
-#npz_compare.py shufflenet_in_fp32.npz $REGRESSION_PATH/shufflenet_v2/data/shufflenet_in_fp32.npz
-cp $REGRESSION_PATH/shufflenet_v2/data/shufflenet_in_fp32.npz shufflenet_in_fp32.npz
 
 # VERDICT
 echo $0 PASSED
