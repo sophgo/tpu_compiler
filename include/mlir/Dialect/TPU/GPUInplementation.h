83d6fb25b6c7f4943088de50bb917a9d744fb35e
sam.zheng <sam.zheng@wisecore.com.tw>
2020-04-06 20:56:18 +0800

    fix conv with bias update cudnn check header
    Change-Id: Idc417c48df0ac37096083078c9de5902aa1126b8

--- include/mlir/Dialect/TPU/GPUInplementation.h
+++ include/mlir/Dialect/TPU/GPUInplementation.h
@@ -6,6 +6,7 @@
 #include <cudnn.h>
 #include <iostream>
 #include <vector>
+
 #define CUDA_KERNEL_LOOP(i, n)                                                 \
   for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n);                 \
        i += blockDim.x * gridDim.x)
@@ -21,19 +22,67 @@
   {                                                                            \
     cudaError_t err = (f);                                                     \
     if (err != cudaSuccess) {                                                  \
-      std::cout << "    Error occurred: " << err << std::endl;                 \
+      std::cout << "CUDA CALL Error occurred: " << err << std::endl;           \
       std::exit(1);                                                            \
     }                                                                          \
   }
+
+#define CUDNN_VERSION_MIN(major, minor, patch)                                 \
+  (CUDNN_VERSION >= (major * 1000 + minor * 100 + patch))
+
+inline const char *cudnnGetErrorString(cudnnStatus_t status) {
+  switch (status) {
+  case CUDNN_STATUS_SUCCESS:
+    return "CUDNN_STATUS_SUCCESS";
+  case CUDNN_STATUS_NOT_INITIALIZED:
+    return "CUDNN_STATUS_NOT_INITIALIZED";
+  case CUDNN_STATUS_ALLOC_FAILED:
+    return "CUDNN_STATUS_ALLOC_FAILED";
+  case CUDNN_STATUS_BAD_PARAM:
+    return "CUDNN_STATUS_BAD_PARAM";
+  case CUDNN_STATUS_INTERNAL_ERROR:
+    return "CUDNN_STATUS_INTERNAL_ERROR";
+  case CUDNN_STATUS_INVALID_VALUE:
+    return "CUDNN_STATUS_INVALID_VALUE";
+  case CUDNN_STATUS_ARCH_MISMATCH:
+    return "CUDNN_STATUS_ARCH_MISMATCH";
+  case CUDNN_STATUS_MAPPING_ERROR:
+    return "CUDNN_STATUS_MAPPING_ERROR";
+  case CUDNN_STATUS_EXECUTION_FAILED:
+    return "CUDNN_STATUS_EXECUTION_FAILED";
+  case CUDNN_STATUS_NOT_SUPPORTED:
+    return "CUDNN_STATUS_NOT_SUPPORTED";
+  case CUDNN_STATUS_LICENSE_ERROR:
+    return "CUDNN_STATUS_LICENSE_ERROR";
+#if CUDNN_VERSION_MIN(6, 0, 0)
+  case CUDNN_STATUS_RUNTIME_PREREQUISITE_MISSING:
+    return "CUDNN_STATUS_RUNTIME_PREREQUISITE_MISSING";
+#endif
+#if CUDNN_VERSION_MIN(7, 0, 0)
+  case CUDNN_STATUS_RUNTIME_IN_PROGRESS:
+    return "CUDNN_STATUS_RUNTIME_IN_PROGRESS";
+  case CUDNN_STATUS_RUNTIME_FP_OVERFLOW:
+    return "CUDNN_STATUS_RUNTIME_FP_OVERFLOW";
+#endif
+  }
+  return "Unknown cudnn status";
+}
 
 #define CUDNN_CALL(f)                                                          \
   {                                                                            \
     cudnnStatus_t err = (f);                                                   \
     if (err != CUDNN_STATUS_SUCCESS) {                                         \
-      std::cout << "    Error occurred: " << err << std::endl;                 \
+      std::cout << "cudnn Error occurred: " << cudnnGetErrorString(err)        \
+                << std::endl;                                                  \
       std::exit(1);                                                            \
     }                                                                          \
   }
+
+#define CUDNN_CHECK(condition)                                                 \
+  do {                                                                         \
+    cudnnStatus_t status = condition;                                          \
+    std::cout << "cudnn check " << cudnnGetErrorString(status) << std::endl;                     \
+  } while (0)
 
 // CUDA: use 512 threads per block
 const int CUDA_NUM_THREADS = 512;
--- lib/Dialect/TPU/Interpreter/GPUImplementation.cu
+++ lib/Dialect/TPU/Interpreter/GPUImplementation.cu
@@ -14,7 +14,7 @@ int gpu_conv(float *input, float *weight, float *bias,
     int kh, int kw, int sh, int sw, int dh, int dw, int ph, int pw, int g){
     cudnnHandle_t cudnn;
     CUDNN_CALL(cudnnCreate(&cudnn));
-
+    // input
     cudnnTensorDescriptor_t in_desc;
     CUDNN_CALL(cudnnCreateTensorDescriptor(&in_desc));
     CUDNN_CALL(cudnnSetTensor4dDescriptor(
@@ -24,6 +24,7 @@ int gpu_conv(float *input, float *weight, float *bias,
     CUDA_CALL(cudaMallocManaged(&in_data, in * ic * ih * iw * sizeof(float)));
     CUDA_CALL(cudaMemcpy(in_data, input, in * ic * ih * iw * sizeof(float),
                cudaMemcpyDefault));
+    // filter
     cudnnFilterDescriptor_t filt_desc;
     CUDNN_CALL(cudnnCreateFilterDescriptor(&filt_desc));
     CUDNN_CALL(cudnnSetFilter4dDescriptor(filt_desc, CUDNN_DATA_FLOAT,
@@ -37,14 +38,14 @@ int gpu_conv(float *input, float *weight, float *bias,
 
     cudnnConvolutionDescriptor_t conv_desc;
     CUDNN_CALL(cudnnCreateConvolutionDescriptor(&conv_desc));
-    CUDNN_CALL(cudnnSetConvolution2dDescriptor(
-        conv_desc, ph, pw, sh, sw, dh, dw, CUDNN_CONVOLUTION,
-        CUDNN_DATA_FLOAT));
+    CUDNN_CALL(cudnnSetConvolution2dDescriptor(conv_desc, ph, pw, sh, sw, dh,
+                                                dw, CUDNN_CONVOLUTION,
+                                                CUDNN_DATA_FLOAT));
 
     cudnnTensorDescriptor_t out_desc;
     CUDNN_CALL(cudnnCreateTensorDescriptor(&out_desc));
     CUDNN_CALL(cudnnSetTensor4dDescriptor(out_desc, CUDNN_TENSOR_NCHW,
-                                          CUDNN_DATA_FLOAT, in, oc, oh, ow));
+                                           CUDNN_DATA_FLOAT, in, oc, oh, ow));
     float *out_data;
     CUDA_CALL(cudaMallocManaged(&out_data, in * oc * oh * ow * sizeof(float)));
 
@@ -59,14 +60,35 @@ int gpu_conv(float *input, float *weight, float *bias,
         cudnn, in_desc, filt_desc, conv_desc, out_desc, algo, &ws_size));
     float *ws_data;
     CUDA_CALL(cudaMallocManaged(&ws_data, ws_size));
-    const float alpha = 1, beta = 0;
+    float alpha = 1, beta = 0;
     CUDNN_CALL(cudnnConvolutionForward(
         cudnn, &alpha, in_desc, in_data, filt_desc, filt_data, conv_desc, algo,
         ws_data, ws_size, &beta, out_desc, out_data));
 
-    CUDA_CALL(cudaMemcpy(output, out_data, oc * oc * oh * ow * sizeof(float),
-                         cudaMemcpyDefault));
+    if (bias) {
+      beta = 1.0f;
+      cudnnTensorDescriptor_t bias_desc;
+      CUDNN_CALL(cudnnCreateTensorDescriptor(&bias_desc));
+      CUDNN_CALL(cudnnSetTensor4dDescriptor(bias_desc, CUDNN_TENSOR_NCHW,
+                                            CUDNN_DATA_FLOAT, 1, oc, 1, 1));
 
+      float *biasdata;
+      CUDA_CALL(cudaMallocManaged(&biasdata, oc * sizeof(float)));
+      CUDA_CALL(cudaMemcpy(biasdata, bias, oc * sizeof(float),
+                           cudaMemcpyDefault));
+    
+      // add bias
+      CUDNN_CHECK(cudnnAddTensor(cudnn, &alpha, bias_desc, biasdata, &beta,
+                                out_desc, out_data));
+
+      CUDNN_CALL(cudnnDestroyTensorDescriptor(bias_desc));
+      CUDA_CALL(cudaFree(biasdata));
+    }
+
+    CUDA_CALL(cudaMemcpy(output, out_data, in * oc * oh * ow * sizeof(float),
+                         cudaMemcpyDefault));
+    cudaDeviceSynchronize();
+    // release
     CUDA_CALL(cudaFree(ws_data));
     CUDA_CALL(cudaFree(out_data));
     CUDNN_CALL(cudnnDestroyTensorDescriptor(out_desc));
