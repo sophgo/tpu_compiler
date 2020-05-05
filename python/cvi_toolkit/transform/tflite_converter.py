8768cb5c5b05fc4014b0b356b19e8bd7b8a7b33f
sam.zheng <sam.zheng@wisecore.com.tw>
2020-05-05 19:22:04 +0800

    add tflite, convop, mulop, padop, addop
    Change-Id: Id2394c4f09bd150da41e0e11b0bf15e72ec6079f

--- python/cvi_toolkit/transform/tflite_converter.py
+++ python/cvi_toolkit/transform/tflite_converter.py
@@ -1,4 +1,5 @@
-from .mlirimporter import BaseConverterInterface, MLIRImporter, checkKey
+from .mlirimporter import MLIRImporter, checkKey
+from .BaseConverter import BaseConverter, TensorType
 from termcolor import colored, cprint
 from math import floor, ceil
 from numbers import Number
@@ -9,8 +10,14 @@ from .utils import calcConv2DSpatial, calcPool2DFloor, calcPool2DCeil, \
 from ..utils.log_setting import setup_logger
 
 # tflite gen by flatbuffer
-from tflite.Model import Model
+
 from tflite.BuiltinOperator import BuiltinOperator
+from tflite.Conv2DOptions import Conv2DOptions
+from tflite.Model import Model
+from tflite.Padding import Padding
+from tflite.Pool2DOptions import Pool2DOptions
+from tflite.Tensor import Tensor as TFL_TENSOR
+
 
 import logging
 import numpy as np
@@ -19,10 +26,6 @@ import operator
 logger = setup_logger('root')
 
 log_flag = logger.level <= logging.INFO
-
-class TensorType(Enum):
-    ACTIVATION = 'ACTIVATION'
-    TENSOR = 'TENSOR'
 
 op_type_id = {
     BuiltinOperator.ADD: "ADD",
@@ -42,6 +45,24 @@ def np_uint8_to_int32(uint8_arr):
     int32_arr = np.frombuffer(uint8_arr.tobytes(), dtype=np.int32)
     return int32_arr
 
+def get_tensor_shape(tensor):
+    if not isinstance(tensor, TFL_TENSOR): raise RuntimeError("Tensor is wrong type")
+
+    x = [tensor.Shape(i) for i in range(tensor.ShapeLength())]
+    if tensor.ShapeLength() == 4:
+        """
+            In tflite define is NHWC
+            return List of NCHW
+        """
+        return [x[0], x[3], x[1], x[2]]
+    elif tensor.ShapeLength() == 2 or tensor.ShapeLength() == 1:
+        return x
+    else:
+        raise ValueError("TODO this case shape len is {}".format(tensor.ShapeLength()))
+
+
+
+
 class TFLiteNode():
     def __init__(self, name, type_id, inputs, outputs, proto):
         self.name = str(name)
@@ -59,10 +80,14 @@ class TFLiteNode():
         cprint("    outputs: {}".format(self.outputs), 'white')
 
 class TFLiteTensor():
-    def __init__(self, name, value, shape):
-        self.name = name
+    def __init__(self, name, value, shape, op_type):
+        if isinstance(name, int):
+            self.name = str(name)
+        else:
+            self.name = name
         self.tensor_data = value
         self.shape = shape
+        self.op_type = op_type
 
     def print_info(self):
         cprint("tensor: {}".format(self.name), 'cyan')
@@ -70,8 +95,9 @@ class TFLiteTensor():
 
 
 
-class TFLiteConverter(BaseConverterInterface):
+class TFLiteConverter(BaseConverter):
     def __init__(self, model_name, tflite_model_file, mlir_file_path):
+        super().__init__()
         self.model_name = model_name
 
         # read tflite model
@@ -108,8 +134,10 @@ class TFLiteConverter(BaseConverterInterface):
         self.init_importer()
         self.output_tensor_file = "{}_1_06eeeb7e.npz".format(model_name)
         self.tfliteop_factory = {
+            "ADD": lambda node: self.convert_add_op(node),
             "PAD" : lambda node: self.convert_pad_op(node),
             "CONV_2D": lambda node: self.convert_conv_op(node),
+            "MAX_POOL_2D": lambda node: self.convert_maxpool_op(node)
         }
 
     def init_importer(self):
@@ -117,38 +145,66 @@ class TFLiteConverter(BaseConverterInterface):
         # get input shape
         inputs = list()
         for input_id in self.input_nodes:
-            input_tensor = self.tflite_graph.Tensors(input_id)
-            input_shape = [input_tensor.Shape(i) for i in range(input_tensor.ShapeLength())]
+            input_shape, _ = self.get_tensor_shape_and_data(input_id)
             inputs.append(input_shape)
         # get output shape
         outputs = list()
         for output_id in self.output_nodes:
-            output_tensor = self.tflite_graph.Tensors(output_id)
-            output_shape = [output_tensor.Shape(i) for i in range(output_tensor.ShapeLength())]
+            output_shape, _ = self.get_tensor_shape_and_data(input_id)
             outputs.append(output_shape)
 
         # init importer
         self.CVI = MLIRImporter(inputs, outputs)
 
-    def get_tflite_tensor_shape(self, tensor_index):
-        tensor = self.tflite_graph.Tensors(tensor_index)
+    def get_tflite_tensor_shape(self, tensor):
+        """
+            Get TFLite Tensor Shape
+            Input: tflite.Tensor.Tensor
+            return:
+                  List, (NHWC) if len is 4
+        """
         return [tensor.Shape(i) for i in range(tensor.ShapeLength())]
 
-    def get_tflite_tensor_data(self, tensor_index):
-        tensor = self.tflite_graph.Tensors(tensor_index)
+    def get_tflite_tensor_data(self, tensor):
+        """
+            Get TFLite Tensor Data
+            Input: tflite.Tensor.Tensor
+            return:
+                1d np.ndarray with each element is one byte, if no data, return 0
+        """
         return self.tflite_model.Buffers(tensor.Buffer()).DataAsNumpy()
 
-    def addOperand(self, op_name, op, shape, tensor_type):
-        cprint("add opernand name: {}\nshape: {} \ntesnor_type {}".format(op_name, shape, tensor_type), "yellow")
-        self.valueMap[op_name] = (op, shape, tensor_type)
+    def get_tensor_shape_and_data(self, tensor_index, data_type=np.float32):
+        """
+            Get TFLite Tensor Shape and Data
+            Input: tensor index
+            optional: data_type, default is np.float32
+            return:
+                List, shape
+                ndarray, data, if no data, return None
+        """
+        tensor = self.tflite_graph.Tensors(tensor_index)
+        shape = self.get_tflite_tensor_shape(tensor)
+        data = self.get_tflite_tensor_data(tensor)
 
-    def getOperand(self, op_name):
-        print(self.valueMap)
-        return self.valueMap[op_name]
+        HAS_DATA = not isinstance(data, int)
+        if HAS_DATA:
+            data = np.frombuffer(data.tobytes(), dtype=data_type)
+            data = data.reshape(tuple(shape))
 
-    def addTensor(self, op_name, tensor_data, tensor_shape):
-        #cprint("add tensor, name: {}\ntensor data: {}".format(op_name, tensor_data), "yellow")
-        self.converted_tensors.append(TFLiteTensor(op_name, tensor_data, tensor_shape))
+        if len(shape) == 4:
+            """
+                In tflite define is NHWC
+                return List of NCHW
+            """
+            return [shape[0], shape[3], shape[1], shape[2]], np.transpose(data, (0,3,1,2)) if HAS_DATA else None
+        elif len(shape) == 2 or len(shape) == 1:
+            return shape, data if HAS_DATA else None
+        else:
+            raise ValueError("TODO this case shape len is {}".format(len(shape)))
+
+    def addTensor(self, op_name, tensor_data, tensor_shape, op_type):
+        self.converted_tensors.append(TFLiteTensor(op_name, tensor_data, tensor_shape, op_type))
 
     def getTensor(self, op_name):
         find_tensor = [t for t in self.converted_tensors if t.name == op_name]
@@ -156,6 +212,12 @@ class TFLiteConverter(BaseConverterInterface):
             raise KeyError("No {} tensor in model".format(op_name))
         else:
             return find_tensor[0]
+
+    def createLoadWeightOp(self, tensor_idx, tensor_name):
+        shape, data = self.get_tensor_shape_and_data(tensor_idx)
+        self.addTensor(tensor_name, data, shape, None)
+        weight_op = self.CVI.add_load_file_op(tensor_name, shape)
+        return weight_op, shape
 
     def TensortoNpz(self):
         tensor_npz = {}
@@ -190,7 +252,7 @@ class TFLiteConverter(BaseConverterInterface):
         # add input op
         for idx, input in enumerate(self.input_nodes):
             input_tensor_idx = input
-            input_shape = self.get_tflite_tensor_shape(input_tensor_idx)
+            input_shape, _ = self.get_tensor_shape_and_data(input_tensor_idx)
             input_op = self.CVI.add_input_op(str(input_tensor_idx), idx)
             self.addOperand(input_tensor_idx, input_op, input_shape, TensorType.ACTIVATION)
 
@@ -215,122 +277,153 @@ class TFLiteConverter(BaseConverterInterface):
         with open(self.mlir_file_path, "w") as f:
             f.write(mlir_txt)
 
+    def convert_add_op(self, node):
+        assert(node.op_type == "ADD")
+        op1, input_shape1, _ = self.getOperand(str(node.inputs[0]))
+        op2, input_shape2, _ = self.getOperand(str(node.inputs[1]))
+        if input_shape1 != input_shape2:
+                raise AttributeError("{} v.s. {} shape not same".format(input_shape1, input_shape2))
+
+        operands = list()
+        operands.append(op1)
+        operands.append(op2)
+        output_shape = input_shape1
+
+        add_op = self.CVI.add_eltwise_add_op("{}_{}".format(node.name, node.op_type), operands, output_shape)
+        self.addOperand(node.name, add_op, output_shape, TensorType.ACTIVATION)
 
     def convert_pad_op(self, node):
         assert(node.op_type == "PAD")
+        """
+            Fix: our mlir don't have padding op,
+            We fuse with next Conv2d
+            In tensorflow official resnet case, it can be work
+            other case is TODO
+            by Sam
+        """
         # first input is activate, second is tensor
         assert(len(node.inputs) == 2)
-        op, shape, _ = self.getOperand(node.inputs[0])
+        op, shape, _ = self.getOperand(str(node.inputs[0]))
 
         padding_attr_tensor_idx = node.inputs[1]
-        padding_attr_shape = self.get_tflite_tensor_shape(padding_attr_tensor_idx)
-        padding_attr_data = self.get_tflite_tensor_data(padding_attr_tensor_idx)
-        padding_attr_data = np.frombuffer(padding_attr_data.tobytes(), dtype=np.int32)
-        padding_attr_data = padding_attr_data.reshape(tuple(padding_attr_shape))
-        # Todo: add mlir with pad op
-        self.addOperand(node.name, None, shape, TensorType.ACTIVATION)
+        padding_attr_shape , padding_attr_data = self.get_tensor_shape_and_data(padding_attr_tensor_idx, data_type=np.int32)
+
+        self.addOperand(node.name, op, shape, TensorType.ACTIVATION)
+        # For Conv2d Get this data
+        self.addTensor(node.name, padding_attr_data, shape, "PAD")
 
     def convert_conv_op(self, node):
         assert(node.op_type == "CONV_2D")
 
         op, shape, _ = self.getOperand(str(node.inputs[0]))
+        # Check if input is padding op
+        padding_data = None
+        try:
+            input_tensor = self.getTensor(str(node.inputs[0]))
+            # Only padding case we handle
+            assert(input_tensor.op_type == "PAD")
+            padding_data = input_tensor.tensor_data
+        except KeyError as k:
+            # Not padding op
+            pass
         operands = list()
         operands.append(op)
+
         # filter
         filter_tensor_idx = node.inputs[1]
-        filter_shape = self.get_tflite_tensor_shape(filter_tensor_idx)
-        filter_data = self.get_tflite_tensor_data(filter_tensor_idx)
-        filter_data = np.frombuffer(filter_data.tobytes(), dtype=np.float32)
         filter_name = "{}_add_weight".format(filter_tensor_idx)
-        self.addTensor(filter_name, filter_data, filter_shape)
-        filter_op = self.CVI.add_load_file_op(filter_name, filter_shape)
+        filter_op, filter_shape = self.createLoadWeightOp(filter_tensor_idx, filter_name)
         operands.append(filter_op)
 
         # bias
         do_bias = len(node.inputs) == 3
         if do_bias:
             bias_tensor_idx = node.inputs[2]
-            bias_shape = self.get_tflite_tensor_shape(bias_tensor_idx)
-            bias_data = self.get_tflite_tensor_data(bias_tensor_idx)
-            bias_data = np.frombuffer(bias_data.tobytes(), dtype=np.float32)
             bias_name = "{}_add_bias".format(bias_tensor_idx)
-            self.addTensor(bias_name, bias_data, bias_shape)
-            bias_op = self.CVI.add_load_file_op(bias_name, bias_shape)
+            bias_op, bias_shape = self.createLoadWeightOp(bias_tensor_idx, bias_name)
             operands.append(bias_op)
 
         op_build_info = node.proto.BuiltinOptions()
         # Parse the Table of options.
-        conv_table = tflite.Conv2DOptions()
+        conv_table = Conv2DOptions()
         conv_table.Init(op_build_info.Bytes, op_build_info.Pos)
-
 
         conv_param = {
             'stride_h': conv_table.StrideH(),
             'stride_w': conv_table.StrideW(),
-            'padding': "SAME" if conv_table.Padding() == tflite.Padding.SAME else "VALUE",
-            'dilation_h': onnx_node.attrs['dilations'][0],
-            'dilation_w': onnx_node.attrs['dilations'][1],
-            'group': onnx_node.attrs['group'],
+            'padding': "SAME" if conv_table.Padding() == Padding.SAME and isinstance(padding_data, np.ndarray) else "VALUE",
+            'dilation_h': conv_table.DilationHFactor(),
+            'dilation_w': conv_table.DilationWFactor(),
+            'group': 1, # Don't have group option?
             'is_dw': False,
-            'with_bias': len(onnx_node.inputs) > 2,
+            'with_bias': len(node.inputs) > 2,
+            'do_relu': False,
+        }
+        on = shape[0]
+        oc = filter_shape[0] # feature map size
+        # padding data order is NHWC
+        oh = calcConv2DSpatial(
+            shape[2],
+            filter_shape[2],
+            conv_param['stride_h'],
+            padding_data[1][0] if isinstance(padding_data, np.ndarray) else 0,
+            conv_param['dilation_h'],
+        )
+        ow = calcConv2DSpatial(
+            shape[3],
+            filter_shape[3],
+            conv_param['stride_w'],
+            padding_data[2][0] if isinstance(padding_data, np.ndarray) else 0,
+            conv_param['dilation_w'],
+        )
+        print(conv_param)
+        output_shape = [on, oc, oh, ow]
+        conv_op = self.CVI.add_conv_op("{}_{}".format(node.name, node.op_type), operands, output_shape, **conv_param)
+        self.addOperand(node.name, conv_op, output_shape, TensorType.ACTIVATION)
+
+    def convert_maxpool_op(self, node):
+        assert(node.op_type == "MAX_POOL_2D")
+
+        op, shape, _ = self.getOperand(str(node.inputs[0]))
+        # Check if input is padding op
+        padding_data = None
+        try:
+            input_tensor = self.getTensor(str(node.inputs[0]))
+            # Only padding case we handle
+            assert(input_tensor.op_type == "PAD")
+            padding_data = input_tensor.tensor_data
+        except KeyError as k:
+            # Not padding op
+            pass
+
+        operands = list()
+        operands.append(op)
+
+        op_build_info = node.proto.BuiltinOptions()
+        pool_table = Pool2DOptions()
+        pool_table.Init(op_build_info.Bytes, op_build_info.Pos)
+
+        pool_max_2d_param = {
+            'stride_h': pool_table.StrideH(),
+            'stride_w': pool_table.StrideW(),
+            'kernel_h': pool_table.FilterWidth(),
+            'kernel_w': pool_table.FilterHeight(),
+            'padding_b': padding_data[1][0] if isinstance(padding_data, np.ndarray) else 0,
+            'padding_r': padding_data[2][0] if isinstance(padding_data, np.ndarray) else 0,
+            'padding_t': padding_data[1][1] if isinstance(padding_data, np.ndarray) else 0,
+            'padding_l': padding_data[2][1] if isinstance(padding_data, np.ndarray) else 0,
             'do_relu': False,
         }
 
-
-        # with_bias = False
-        # if (len(onnx_node.inputs) == 3):
-        #     #with bias
-        #     with_bias = True
-        #     bias_name = onnx_node.inputs[2]
-        #     bias_tensor = self.getTensor(bias_name)
-        # conv_param = {
-        #     'stride_h':  onnx_node.attrs['strides'][0],
-        #     'stride_w':  onnx_node.attrs['strides'][1],
-        #     'padding': "SAME" if onnx_node.attrs['pads'][0] > 0 else "VALID",
-        #     'dilation_h': onnx_node.attrs['dilations'][0],
-        #     'dilation_w': onnx_node.attrs['dilations'][1],
-        #     'group': onnx_node.attrs['group'],
-        #     'is_dw': False,
-        #     'with_bias': len(onnx_node.inputs) > 2,
-        #     'do_relu': False,
-        # }
-
-        # on = shape[0]
-        # oc = filter_tensor.shape[0] # feature map size
-        # oh = calcConv2DSpatial(
-        #     shape[2],
-        #     onnx_node.attrs['kernel_shape'][0],
-        #     onnx_node.attrs['strides'][0],
-        #     onnx_node.attrs['pads'][0],
-        #     onnx_node.attrs['dilations'][0]
-        # )
-        # ow = calcConv2DSpatial(
-        #     shape[3],
-        #     onnx_node.attrs['kernel_shape'][1],
-        #     onnx_node.attrs['strides'][1],
-        #     onnx_node.attrs['pads'][1],
-        #     onnx_node.attrs['dilations'][1]
-        # )
-
-        # if conv_param['group'] != 1:
-        #     # filter shape s is in (g, oc/g, ic/g, kh, kw)
-        #     g = conv_param['group']
-        #     ic = shape[1]
-        #     kh = onnx_node.attrs['kernel_shape'][0]
-        #     kw = onnx_node.attrs['kernel_shape'][1]
-        #     new_shape = [g, int(oc/g), int(ic/g), kh, kw]
-        #     filter_op = self.CVI.add_load_file_op(filter_tensor.name, new_shape)
-        # else:
-        #     filter_op = self.CVI.add_load_file_op(filter_tensor.name, filter_shape)
-        # operands.append(filter_op)
-
-        # if with_bias:
-        #     bias_op = self.CVI.add_load_file_op(bias_name, bias_tensor.shape)
-        #     operands.append(bias_op)
-
-        # output_shape = [on, oc, oh, ow]
-        # conv_op = self.CVI.add_conv_op("{}_{}".format(onnx_node.name, onnx_node.op_type), operands, output_shape, **conv_param)
-        # self.addOperand(onnx_node.name, conv_op, output_shape, TensorType.ACTIVATION)
+        operands = list()
+        operands.append(op)
+        on = shape[0]
+        oc = shape[1]
+        oh = calcPool2DFloor(shape[2], pool_max_2d_param['kernel_h'], pool_max_2d_param['stride_h'], pool_max_2d_param['padding_b'])
+        ow = calcPool2DFloor(shape[3], pool_max_2d_param['kernel_w'], pool_max_2d_param['stride_w'], pool_max_2d_param['padding_r'])
+        output_shape = [int(on), int(oc), int(oh), int(ow)]
+        pool_max_op = self.CVI.add_pool_max_2d_op("{}_{}".format(node.name, node.op_type), operands, output_shape, **pool_max_2d_param)
+        self.addOperand(node.name, pool_max_op, output_shape, TensorType.ACTIVATION)
 
     def run(self):
         self.convert_node()
