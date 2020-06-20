ed0e63f941183e6e00525589894a7a78e3384f00
charle.hu <charle.hu@cvitek.com>
2020-06-20 22:37:34 +0800

    add caffe python frontend
    1. add caffe python frontend
    2. support resnet now, other caffemodel will support later
    
    Change-Id: Ia73a08241e04cc05f0ced017be6529c8c58ada90

--- python/cvi_toolkit/cvi_model_convert.py
+++ python/cvi_toolkit/cvi_model_convert.py
@@ -4,10 +4,12 @@ import argparse
 from cvi_toolkit.transform.onnx_converter import OnnxConverter
 from cvi_toolkit.transform.tflite_converter import TFLiteConverter
 from cvi_toolkit.transform.tensorflow_converter import TFConverter
+from cvi_toolkit.transform.caffe_converter import CaffeConverter
 from cvi_toolkit.utils.log_setting import setup_logger
 
 logger = setup_logger('root', log_level="INFO")
 CVI_SupportFramework = [
+    "caffe",
     "onnx",
     "tflite",
     "tensorflow",
@@ -26,6 +28,8 @@ def Convert(args):
             args.model_name, args.model_path, args.mlir_file_path)
     elif args.model_type == "tensorflow":
         c = TFConverter(args.model_name, args.model_path, args.mlir_file_path, batch_size=args.batch_size)
+    elif args.model_type == "caffe":
+        c = CaffeConverter(args.model_name, args.model_path, args.model_dat, args.mlir_file_path, batch_size=args.batch_size)
     c.run()
 
 
@@ -42,6 +46,11 @@ def main():
     parser.add_argument(
         "--model_type",
         help="model, type ex. {}".format(CVI_SupportFramework)
+    )
+    parser.add_argument(
+        "--model_dat",
+        help="model weights, caffe need",
+        default=""
     )
     parser.add_argument(
         "--mlir_file_path",
