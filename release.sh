2d040dca80a1b23ceb40e23b0e570469eca4e025
Lei Wang <lei.wang@cvitek.com>
2020-07-04 18:16:34 +0800

    fix path error, and only pack cvimodel_regression for 18.04 build
    Change-Id: I5de22b15f58cff1df16aec2191f8019037f0ffd4

--- release.sh
+++ release.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
+#
+# Run after running run_regression.sh -e
+#
+
 # "/home/ftp/mlir/daily_build"
-REL_DIR=$1
-dest_dir=$REL_DIR/$(date '+%Y-%m-%d')
+local RELEASE_DIR=$1
+local REGRESSION_DIR=$2
+local dest_dir=$RELEASE_DIR/$(date '+%Y-%m-%d')
 rm -rf $dest_dir
 mkdir -p $dest_dir
 
@@ -16,18 +21,31 @@ if [ ! -e $INSTALL_PATH ]; then
   echo "INSTALL_PATH=$INSTALL_PATH not exist"
   exit 1
 fi
+if [ ! -e $REGRESSION_DIR ]; then
+  echo "REGRESSION_DIR=$REGRESSION_DIR not exist"
+  exit 1
+fi
+if [ ! -e $REGRESSION_DIR/cvimodel_regression ]; then
+  echo "$REGRESSION_DIR/cvimodel_regression not exist"
+  exit 1
+fi
 
 ###########################################################
 # pack cvitek_mlir
 ###########################################################
+local os_ver=$( lsb_release -sr )
 pushd $INSTALL_PATH/..
 if [ ! -e ./cvitek_mlir ]; then
   echo "./cvitek_mlir not exist"
   exit 1
 fi
-os_ver=$( lsb_release -sr )
 tar zcvf $dest_dir/cvitek_mlir_ubuntu-${os_ver}.tar.gz cvitek_mlir
 popd
+
+# exit if os_ver is not 18.04
+if [ "$os_ver" != "18.04" ]; then
+  exit 0
+fi
 
 ###########################################################
 # pack cvimodel_samples and samples
@@ -37,22 +55,13 @@ tar zcvf $dest_dir/cvimodel_samples.tar.gz cvimodel_samples
 # tar zcvf $dest_dir/cvimodel_release.tar.gz cvimodel_release
 popd
 # tpu_samples
-cp -a $INSTALL_PATH/cvitek_mlir/samples -a ./cvitek_tpu_samples
+cp -a $INSTALL_PATH/samples -a ./cvitek_tpu_samples
 tar zcvf $dest_dir/cvitek_tpu_samples.tar.gz cvitek_tpu_samples
 rm -rf cvitek_tpu_samples
 
 ###########################################################
 # pack regresion cvimodels
 ###########################################################
-REGRESSION_DIR=$2
-if [ ! -e $REGRESSION_DIR ]; then
-  echo "REGRESSION_DIR=$REGRESSION_DIR not exist"
-  exit 1
-fi
-if [ ! -e $REGRESSION_DIR/cvimodel_regression ]; then
-  echo "$REGRESSION_DIR/cvimodel_regression not exist"
-  exit 1
-fi
 pushd $REGRESSION_DIR
 # generate int8 input data (for bs1 only)
 pushd cvimodel_regression
