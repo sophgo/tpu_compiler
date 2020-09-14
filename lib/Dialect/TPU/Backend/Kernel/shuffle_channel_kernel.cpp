166b46ec1a932d2fd5a5c56cf7227030ed52d612
arvin.chou <arvin.chou@cvitek.com>
2020-09-14 12:00:02 +0800

    [mlir] rewrite tg shuffle channel without s2s
    Symptom: s2s impact performance
    Root cause:
    Solution: recommend rewrite to s2l->l2s with
    contiguous memory layout
    Note:
    1. verify under shufflenet_v2
    2. no increase performance under shufflenet_v2
    you can refer https://cvitekcn.sharepoint.com/:x:/s/msteams_60626d/EYOpBkroIA-kHhxIAcQ_AfQBUeH4G5OIv_-ma5jnNMYokQ?e=QdzL99
    for more details
    
    Change-Id: I14fd9e1f67723e0df5b0a35914984d794d37db10

--- lib/Dialect/TPU/Backend/Kernel/shuffle_channel_kernel.cpp
+++ lib/Dialect/TPU/Backend/Kernel/shuffle_channel_kernel.cpp
@@ -17,7 +17,8 @@ void cvi_backend_tg_shuffle_channel_kernel(
     const CviBackendContext &ctx, uint32_t stream_id, uint32_t inst_id, uint32_t layer_id,
     const uint32_t *depends, uint32_t depends_len, gaddr_t input_gaddr,
     gaddr_t output_gaddr, int batch, int channel, int frame_size, int group,  cvk_fmt_t fmt) {
-  frame_size *= ((fmt == CVK_FMT_BF16) ? sizeof(uint16_t) : sizeof(uint8_t));
+  int unit_size = ((fmt == CVK_FMT_BF16) ? sizeof(uint16_t) : sizeof(uint8_t));
+  frame_size *= unit_size;
   uint32_t feature_size = (uint32_t)(channel * frame_size);
   uint32_t col = (uint32_t)(channel / group);
   cvk_tg_shape_t shape = {(uint32_t)batch, (uint32_t)group, (uint32_t)col, (uint32_t)frame_size};
@@ -33,6 +34,49 @@ void cvi_backend_tg_shuffle_channel_kernel(
   // For tdma
   ctx.set_layer_id(layer_id);
 
+#if 0
+  // keep golden
   tdma_g2g_tensor_copy(ctx, input_gaddr, shape, s_stride, CVK_FMT_I8, output_gaddr,
                        shape, d_stride, CVK_FMT_I8);
+#else
+  shape.n = 1; // TODO: support n dim shift
+  shape.w /= unit_size;
+  std::vector<std::pair<cvk_tl_shape_t, gaddr_t>> tiling_info;
+  tiling_packing(ctx, /*require_shape=*/0, /*coeff_lane_shape=*/0, /*blob_num=*/1,
+      fmt, &tiling_info, TilingDimNH, &shape);
+
+  // accumuate store
+  int output_n = batch;
+  for (int batch = 0; batch < output_n; batch++) {
+    int top_h_shift = 0;
+    int bottom_local_shift = 0;
+    for (size_t i = 0; i < tiling_info.size(); i++) {
+      int n = tiling_info[i].first.n;
+      int c = tiling_info[i].first.c;
+      int h = tiling_info[i].first.h;
+      int w = tiling_info[i].first.w;
+      //gaddr_t gaddr_offset = tiling_info[i].second;
+
+      assert(w == frame_size / unit_size && "not support tiling w");
+
+      // load
+      cvk_tl_shape_t input_shape = ctx.shape_t4(n, c, h, w);
+      cvk_tl_t *bottom = ctx.lmem_alloc_tensor(input_shape, fmt, /*eu_align=*/0);
+      ctx.tdma_load_stride_bf16(bottom,
+          input_gaddr + bottom_local_shift + batch * s_stride.n, s_stride);
+      bottom_local_shift += bottom->stride.c;
+
+      // store back
+      ctx.tdma_store_stride_bf16(bottom,
+          output_gaddr + top_h_shift * d_stride.h + batch * d_stride.n, d_stride,
+          /*do_transpose=*/0);
+
+      // shift gaddr
+      top_h_shift += bottom->shape.h;
+
+      // release
+      ctx.lmem_free_tensor(bottom);
+    }
+  }
+#endif
 }
