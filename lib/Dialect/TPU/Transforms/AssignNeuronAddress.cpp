c44a31d435e119e89f334c227cd78f08b1b6feb7
jiang.jiang <jiang.jiang@cvitek.com>
2020-10-20 17:02:56 +0800

    set different base addr index for inputs & outputs of model
    Change-Id: If53b84c99ef96edf8e4708522547830da1b942b4

--- lib/Dialect/TPU/Transforms/AssignNeuronAddress.cpp
+++ lib/Dialect/TPU/Transforms/AssignNeuronAddress.cpp
@@ -159,7 +159,6 @@ struct TgSliceAddressPattern : public RewritePattern {
   }
 };
 
-
 template <typename OpTy>
 struct TgConcatAddressPattern : public RewritePattern {
   TgConcatAddressPattern(MLIRContext *context)
@@ -189,7 +188,6 @@ struct TgConcatAddressPattern : public RewritePattern {
     return matchSuccess();
   }
 };
-
 
 template <typename OpTy>
 struct TlLgStoreAddressNeuronPattern : public RewritePattern {
@@ -225,7 +223,7 @@ static uint32_t getOpLine(Operation *op) {
   return loc.getLine();
 }
 
-static void findInPlaceOpMaxUsePosition(Operation *op, uint32_t& maxPosition) {
+static void findInPlaceOpMaxUsePosition(Operation *op, uint32_t &maxPosition) {
   for (auto &use : op->getResult(0)->getUses()) {
     Operation *next = use.getOwner();
     if (isInPlaceOp(next)) {
@@ -239,19 +237,95 @@ static void findInPlaceOpMaxUsePosition(Operation *op, uint32_t& maxPosition) {
   }
 }
 
-static bool needAssignNeuronForOp(Operation *op) {
-  if (!isa<tpu::GenericCpuOp>(op) &&
-      !isa<tpu::InputOp>(op)) {
+static bool isOpBelongToTPUPrivateMemoryRegion(Operation *op) {
+  if (!isa<tpu::GenericCpuOp>(op) && !isa<tpu::InputOp>(op)) {
     return true;
   }
   for (auto &use : op->getResult(0)->getUses()) {
     Operation *next = use.getOwner();
-    if (isa<tpu::GenericCpuOp>(next) ||
-        isa<ReturnOp>(next)) {
+    if (isa<tpu::GenericCpuOp>(next) || isa<ReturnOp>(next)) {
       return false;
     }
   }
   return true;
+}
+
+static bool isOpBelongToIOMemoryRegion(std::vector<Operation *> &ioRegion, Operation *op) {
+  // Warning, IO memory region can only has capacity to store 5 ops.
+  if (ioRegion.size() >= 5) {
+    return false;
+  }
+  if (isa<tpu::InputOp>(op)) {
+    for (auto &use : op->getResult(0)->getUses()) {
+      Operation *next = use.getOwner();
+      if (!isa<tpu::GenericCpuOp>(next)) {
+        return true;
+      }
+    }
+  } else if (!isa<tpu::GenericCpuOp>(op)) {
+    auto next = getNextOp(op);
+    if (next && isa<ReturnOp>(next)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+static void
+updateLiveRangeOfOps(FuncOp &fn, std::vector<Operation *> &chosenOps,
+                     std::map<Operation *, std::vector<uint32_t>> &liveRange) {
+
+  auto updateOperandsLiveRange = [&](Operation *op, uint32_t endPosition) {
+    for (uint32_t i = 0; i < op->getNumOperands(); i++) {
+      auto opd = op->getOperand(i)->getDefiningOp();
+      if (liveRange.find(opd) != liveRange.end()) {
+        if (liveRange[opd][1] == 0xFFFFFFFF || liveRange[opd][1] < endPosition) {
+          liveRange[opd][1] = endPosition;
+        }
+      }
+    }
+  };
+
+  fn.walk([&](Operation *op) {
+    uint32_t endPosition = getOpLine(op) + 1;
+    if (isa<tpu::TL_LW_Conv2DOp>(op) || isa<tpu::TL_BroadcastMulOp>(op) ||
+        isa<tpu::TL_EltwiseAddOp>(op) || isa<tpu::TL_EltwiseMulOp>(op) ||
+        isa<tpu::TL_PoolAvg2DOp>(op) || isa<tpu::TL_LutOp>(op)) {
+      bool store = op->getAttr("tl_store_flag").cast<BoolAttr>().getValue();
+      if (store) {
+        chosenOps.push_back(op);
+        liveRange[op] = {getOpLine(op), 0xFFFFFFFF};
+      }
+      updateOperandsLiveRange(op, endPosition);
+    } else if (isInPlaceOp(op)) {
+      uint32_t maxPosition = getOpLine(op) + 1;
+      findInPlaceOpMaxUsePosition(op, maxPosition);
+      updateOperandsLiveRange(op, maxPosition);
+    } else if (isa<tpu::TL_LG_StoreOp>(op)) {
+      auto joinOp = getNextOp(op);
+      if (liveRange.find(joinOp) == liveRange.end()) {
+        chosenOps.push_back(joinOp);
+        liveRange[joinOp] = {getOpLine(op), 0xFFFFFFFF};
+      }
+      updateOperandsLiveRange(op, endPosition);
+    } else if (isa<tpu::InputOp>(op)) {
+      auto nextOp = getNextOp(op);
+      if (!nextOp ||
+          (!isa<tpu::GenericCpuOp>(nextOp) &&
+           !isa<tpu::ReshapeOp>(nextOp))) {
+        chosenOps.push_back(op);
+        liveRange[op] = {getOpLine(op), 0xFFFFFFFF};
+        updateOperandsLiveRange(op, endPosition);
+      }
+    } else if (isa<tpu::GenericCpuOp>(op) ||
+               llvm::dyn_cast<tpu::TpuTGOpCodegenInterface>(op)) {
+      chosenOps.push_back(op);
+      liveRange[op] = {getOpLine(op), 0xFFFFFFFF};
+      updateOperandsLiveRange(op, endPosition);
+    } else {
+      updateOperandsLiveRange(op, endPosition);
+    }
+  });
 }
 
 class AssignNeuronAddressPass : public FunctionPass<AssignNeuronAddressPass> {
@@ -279,117 +353,94 @@ public:
       }
     });
 
-    std::vector<Operation *> ops;
+    /*
+     * find all ops that needed to be assigned,
+     * and update live ranges of all ops in the function.
+     */
+    std::vector<Operation *> chosenOps;
     std::map<Operation *, std::vector<uint32_t>> liveRange;
+    updateLiveRangeOfOps(fn, chosenOps, liveRange);
 
-    auto updateOperandLiveRange = [&](Operation *op, uint32_t endPosition) {
-      for (uint32_t i = 0; i < op->getNumOperands(); i++) {
-        auto opd = op->getOperand(i)->getDefiningOp();
-        if (liveRange.find(opd) != liveRange.end()) {
-          if (liveRange[opd][1] == 0xFFFFFFFF || liveRange[opd][1] < endPosition) {
-            liveRange[opd][1] = endPosition;
-          }
-        }
-      }
-    };
-
-    fn.walk([&](Operation *op) {
-      uint32_t endPosition = getOpLine(op) + 1;
-      if (isa<tpu::TL_LW_Conv2DOp>(op) ||
-          isa<tpu::TL_BroadcastMulOp>(op) ||
-          isa<tpu::TL_EltwiseAddOp>(op) ||
-          isa<tpu::TL_EltwiseMulOp>(op) ||
-          isa<tpu::TL_PoolAvg2DOp>(op) ||
-          isa<tpu::TL_LutOp>(op)) {
-        bool store = op->getAttr("tl_store_flag").cast<BoolAttr>().getValue();
-        if (store) {
-          ops.push_back(op);
-          liveRange[op] = {getOpLine(op), 0xFFFFFFFF};
-        }
-        updateOperandLiveRange(op, endPosition);
-      } else if (isInPlaceOp(op)) {
-        uint32_t maxPosition = getOpLine(op) + 1;
-        findInPlaceOpMaxUsePosition(op, maxPosition);
-        updateOperandLiveRange(op, maxPosition);
-      } else if (isa<tpu::TL_LG_StoreOp>(op)) {
-        auto joinOp = getNextOp(op);
-        if (liveRange.find(joinOp) == liveRange.end()) {
-          ops.push_back(joinOp);
-          liveRange[joinOp] = {getOpLine(op), 0xFFFFFFFF};
-        }
-        updateOperandLiveRange(op, endPosition);
-      } else if (isa<tpu::InputOp>(op)) {
-        auto nextOp = getNextOp(op);
-        if (!nextOp ||
-            (!isa<tpu::GenericCpuOp>(nextOp) &&
-            !isa<tpu::ReshapeOp>(nextOp))) {
-          ops.push_back(op);
-          liveRange[op] = {getOpLine(op), 0xFFFFFFFF};
-          updateOperandLiveRange(op, endPosition);
-        }
-      } else if (isa<tpu::GenericCpuOp>(op) ||
-                 llvm::dyn_cast<tpu::TpuTGOpCodegenInterface>(op)) {
-        ops.push_back(op);
-        liveRange[op] = {getOpLine(op), 0xFFFFFFFF};
-        updateOperandLiveRange(op, endPosition);
-      } else {
-        updateOperandLiveRange(op, endPosition);
-      }
-    });
-
-    int64_t sharedGmemOffset = 0;
-    int64_t sharedGmemSize = 0;
-    int64_t privateGmemSize = 0;
-    std::map<Operation *, int64_t> gaddrMap;
-    std::vector<Operation *> opsOfTpuMainFunc;
-    std::vector<Operation *> opsOfCpuMainFunc;
+    // devide chosen ops to defferent memory regions.
+    std::vector<std::vector<Operation *>> opsInSharedMemoryRegions;
+    std::vector<Operation *> opsInPrivateMemoryRegionForTPU;
+    std::vector<Operation *> opsInPrivateMemoryRegionForCpu;
+    std::vector<Operation *> opsInIOMemoryRegion;
 
     auto subFunctions = SubFunction::divideOpsToSubFunc(&fn);
     for (auto subFn : subFunctions) {
       if (subFn->tpu) {
-        std::vector<Operation *> opsOfSubFunc;
+        std::vector<Operation *> targetOps;
         for (auto op : subFn->ops) {
-          if (isOpInVector(op, ops)) {
+          if (isOpInVector(op, chosenOps)) {
             if (!isOpInVector(op, subFn->outputs)) {
-              opsOfSubFunc.push_back(op);
-            } else if (needAssignNeuronForOp(op)) {
-              opsOfTpuMainFunc.push_back(op);
+              targetOps.push_back(op);
+            } else if (isOpBelongToIOMemoryRegion(opsInIOMemoryRegion, op)) {
+              opsInIOMemoryRegion.push_back(op);
+            } else if (isOpBelongToTPUPrivateMemoryRegion(op)) {
+              opsInPrivateMemoryRegionForTPU.push_back(op);
             } else {
-              opsOfCpuMainFunc.push_back(op);
+              opsInPrivateMemoryRegionForCpu.push_back(op);
             }
           }
         }
-        GmemAllocator allocator(gaddrMap, clNeuronAlignment);
-        auto gmemUsed = allocator.assignGaddr(opsOfSubFunc, liveRange, clNeuronReuse, sharedGmemOffset);
-        if (!clNeuronReuse) {
-          sharedGmemOffset += gmemUsed;
-        }
-        if (sharedGmemSize < sharedGmemOffset + gmemUsed) {
-          sharedGmemSize = sharedGmemOffset + gmemUsed;
-        }
+        opsInSharedMemoryRegions.push_back(targetOps);
       } else {
         for (auto op : subFn->ops) {
-          if (isOpInVector(op, ops)) {
-            if (needAssignNeuronForOp(op)) {
-              opsOfTpuMainFunc.push_back(op);
+          if (isOpInVector(op, chosenOps)) {
+            if (isOpBelongToIOMemoryRegion(opsInIOMemoryRegion, op)) {
+              opsInIOMemoryRegion.push_back(op);
+            } else if (isOpBelongToTPUPrivateMemoryRegion(op)) {
+              opsInPrivateMemoryRegionForTPU.push_back(op);
             } else {
-              opsOfCpuMainFunc.push_back(op);
+              opsInPrivateMemoryRegionForCpu.push_back(op);
             }
           }
         }
       }
     }
+
+    // assign gaddr for ops in defferent memory regions.
+    int64_t sharedGmemOffset = 0;
+    int64_t sharedGmemSize = 0;
+    std::map<Operation *, int64_t> gaddrMap;
+    // 1. Assign gaddr for ops in shared regions.
+    for (auto &targetOps : opsInSharedMemoryRegions) {
+      GmemAllocator allocator(gaddrMap, clNeuronAlignment);
+      auto gmemUsed = allocator.assignGaddr(targetOps, liveRange,
+                                            clNeuronReuse, sharedGmemOffset);
+      if (!clNeuronReuse) {
+        sharedGmemOffset += gmemUsed;
+      }
+      if (sharedGmemSize < sharedGmemOffset + gmemUsed) {
+        sharedGmemSize = sharedGmemOffset + gmemUsed;
+      }
+    }
+
     int64_t baseGaddr = (((uint64_t)2) << 40);
-    GmemAllocator allocator(gaddrMap, clNeuronAlignment);
-    privateGmemSize = allocator.assignGaddr(opsOfCpuMainFunc, liveRange, clNeuronReuse, baseGaddr);
-    GmemAllocator allocator1(gaddrMap, clNeuronAlignment);
-    privateGmemSize += allocator1.assignGaddr(opsOfTpuMainFunc, liveRange, clNeuronReuse, baseGaddr + privateGmemSize);
+    int64_t privateGmemSize = 0;
+    // 2. Assign gaddr for ops in CPU private region.
+    if (!opsInPrivateMemoryRegionForCpu.empty()) {
+      GmemAllocator allocator(gaddrMap, clNeuronAlignment);
+      privateGmemSize = allocator.assignGaddr(opsInPrivateMemoryRegionForCpu, liveRange,
+                                              clNeuronReuse, baseGaddr);
+    }
+    // 3. Assign gaddr for ops in TPU private region.
+    if (!opsInPrivateMemoryRegionForTPU.empty()) {
+      GmemAllocator allocator(gaddrMap, clNeuronAlignment);
+      privateGmemSize += allocator.assignGaddr(opsInPrivateMemoryRegionForTPU, liveRange,
+                                               clNeuronReuse, baseGaddr + privateGmemSize);
+    }
+    // 4. Assign gaddr for ops in IO memory regin.
+    for (int i = 0; i < (int)opsInIOMemoryRegion.size(); ++i) {
+      gaddrMap[opsInIOMemoryRegion[i]] = (((uint64_t)3 + i) << 40);
+    }
 
     fn.setAttr("private_gmem", Builder(context).getI64IntegerAttr(privateGmemSize));
     fn.setAttr("shared_gmem", Builder(context).getI64IntegerAttr(sharedGmemSize));
 
     std::set<Operation *> gmemReusedSet;
-    GmemAllocator::markGmemReusedOp(ops, gaddrMap, gmemReusedSet, clNeuronAlignment);
+    GmemAllocator::markGmemReusedOp(chosenOps, gaddrMap, gmemReusedSet, clNeuronAlignment);
 
     fn.walk([&](Operation *op) {
       if (gaddrMap.find(op) != gaddrMap.end()) {
@@ -415,9 +466,8 @@ public:
           }
           auto &os = neuronMapFile->os();
           os << mlir::getOpName(op) << "," << llvm::format_hex(gaddrMap[op], 10) << ","
-             << dtype << ","
-             << shape[0] << "," << shape[1] << ","
-             << shape[2] << "," << shape[3] << "\n";
+             << dtype << "," << shape[0] << "," << shape[1] << "," << shape[2] << ","
+             << shape[3] << "\n";
         }
       }
     });
@@ -426,13 +476,11 @@ public:
     patterns.insert<TgSliceAddressPattern<tpu::TG_INT8_SliceOp>,
                     TgSliceAddressPattern<tpu::TG_BF16_SliceOp>,
                     TgConcatAddressPattern<tpu::TG_INT8_ConcatOp>,
-                    TgConcatAddressPattern<tpu::TG_BF16_ConcatOp>
-                   >(context);
+                    TgConcatAddressPattern<tpu::TG_BF16_ConcatOp>>(context);
     applyPatternsGreedily(fn, patterns);
     patterns.clear();
     patterns.insert<TlLgStoreAddressNeuronPattern<tpu::TL_LG_StoreOp>,
-                    TlLgLoadNeuronAddressPattern<tpu::TL_LG_LoadNeuronOp>
-                   >(context);
+                    TlLgLoadNeuronAddressPattern<tpu::TL_LG_LoadNeuronOp>>(context);
     applyPatternsGreedily(fn, patterns);
 
     if (neuronMapFile) {
@@ -448,7 +496,6 @@ private:
     }
     return false;
   }
-
 };
 
 } // namespace
