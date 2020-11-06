4adc16a579eac7b3e41845d5e118abd16e8c29c0
quan.xiao <quan.xiao@cvitek.com>
2020-11-06 16:37:33 +0800

    support slice op in layer group
    Change-Id: Ic2eee2a4c3eba858d4037e12bf742b4e630920ac

--- lib/Dialect/TPU/Optimization/LayerGroup/MixNet.hpp
+++ lib/Dialect/TPU/Optimization/LayerGroup/MixNet.hpp
@@ -201,6 +201,14 @@ class MixNet {
                             int timestep_idx,
                             bool is_h_split);
 
+  void _add_tl_slice_op(MixOp * mix_op,
+                       const std::vector<int>& in_tensors,
+                       const std::vector<int>& out_tensors,
+                       net_timestep* time_step,
+                       int timestep_idx,
+                       bool is_h_split);
+
+
   void _add_load_op(int group_idx, int tensor_id,
                     net_timestep* time_step, int timestep_idx);
 
