e37e425f8b5ffa291506c9cef5f1650207e5d2d5
arvin.chou <arvin.chou@cvitek.com>
2021-03-16 13:59:34 +0800

    [mlir] support align corner quant
    Symptom: quant interp op fail
    Root cause: not set properly condition
    Solution: add 'align_corner' case
    Note: NA
    
    Change-Id: If88545b1db19c9ce02efcea1316c4e5a3f0b5edd

--- lib/Dialect/TPU/Quantization/QuantizeInt8.cpp
+++ lib/Dialect/TPU/Quantization/QuantizeInt8.cpp
@@ -1536,7 +1536,8 @@ LogicalResult tpu::InterpOp::quantizeInt8() {
 
   std::string _coordinate_transformation_mode =
       interpOp.coordinate_transformation_mode().str();
-  if (_coordinate_transformation_mode != "half_pixel") {
+  SmallVector<std::string, 3> supported = {"half_pixel", "align_corners", "pytorch_half_pixel"};
+  if (std::find(supported.begin(), supported.end(), _coordinate_transformation_mode) == supported.end()) {
     std::string err_msg = "No support " + _coordinate_transformation_mode
                                         + " mode \n";
     llvm_unreachable(err_msg.c_str());
