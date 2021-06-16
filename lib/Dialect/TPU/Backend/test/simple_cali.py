70a1bdc6ff478fc940c2b619db23850d9ac3309d
charle.hu <charle.hu@cvitek.com>
2021-06-16 17:43:26 +0800

    refine backend test
    adjust threshold table
    
    Change-Id: Ib83528ae0bd5f5515a6d4b03520660f0b9b041a7

--- lib/Dialect/TPU/Backend/test/simple_cali.py
+++ lib/Dialect/TPU/Backend/test/simple_cali.py
@@ -1,9 +1,15 @@
 import numpy as np
-import os
 import sys
 
 npz_file = sys.argv[1]
 
 tensors = np.load(npz_file)
 for t in tensors:
-    print("{} {}".format(t,np.abs(np.max(tensors[t])+0.0001)))
+    threshold = np.abs(np.max(tensors[t]))+0.0001
+    if threshold >= 1000000.0:
+        threshold = 1000000.0
+    elif np.isnan(threshold) or threshold == 0.0:
+        threhsold = 0.1
+    else:
+        pass
+    print("{} {}".format(t,threshold))
